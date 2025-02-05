21a5f648cca3309d3884f295bb918707d984f4b3
erdeep64930 <erdeep64930@gmail.com>
2025-02-05 19:21:55 +0530

    looping has been started

---
+++ kvRao/relationalOperatorEx.py
@@ -0,0 +1,7 @@
+a= int(input("enter first value"))
+b= int(input("enter second value"))
+
+print("*"*50)
+print("Result of relational expression")
+print("*"*50)
+print("\t{}>{}={}".format(a,b,a>b))
